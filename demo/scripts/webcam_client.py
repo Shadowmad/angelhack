18bd3c801419c440d222d363eb7511043270bf7d
alex <alex.izzedin@gmail.com>
2018-04-29 00:07:31 -0700

    reorganized structure

---
+++ demo/scripts/webcam_client.py
@@ -0,0 +1,37 @@
+"""
+Simply display the contents of the webcam with optional mirroring using OpenCV 
+via the new Pythonic cv2 interface.  Press <esc> to quit.
+"""
+
+import cv2
+import socket
+import struct
+import pickle
+
+
+def read_webcam():
+	cam = cv2.VideoCapture(0)
+	clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	clientsocket.connect(('localhost', 8089))
+	while True:
+		ret_val, frame = cam.read()
+		data = pickle.dumps(frame)
+		data = struct.pack("L", len(data)) + data
+		#data = data.encode("utf-8")
+		print(len(data))
+		# sending frame to the server
+		clientsocket.sendall(data)
+
+		# displaying frame on the screen
+		# cv2.imshow('my webcam', frame)
+		# if cv2.waitKey(1) == 27: 
+		# 	break  # esc to quit
+	cv2.destroyAllWindows()
+
+
+def main():
+	read_webcam()
+
+
+if __name__ == '__main__':
+	main()
