a89a7c89df9280b0521be98da8e3f173c359c0f4
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-04-29 00:23:36 -0700

    Mooved mongo to scripts folder

---
+++ demo/scripts/mongo.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+MONGO_URI = 'mongodb://localhost:27017/'
+
+def save_emotion(conn, event, data):
+	db = conn.demo
+	coll = db.emotions
+	result = coll.insert_many(data)
+
+def connectDb():
+	client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=10)
+	try:
+		client.server_info()
+	except:
+		print("Can't connect to db")
+		return
+	print("Connected to db on URI: %s", MONGO_URI)
+	return client
