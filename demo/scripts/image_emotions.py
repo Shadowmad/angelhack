d38382ade06660cce77864049d6e6aec202c06b6
alex <alex.izzedin@gmail.com>
2018-04-29 10:40:03 -0700

    fast key added

--- demo/scripts/image_emotions.py
+++ demo/scripts/image_emotions.py
@@ -5,10 +5,10 @@ import time
 import mongo
 
 images_dir = "../storage/images/"
-url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=false&returnFaceLandmarks=false&returnFaceAttributes=emotion'
+face_recognition_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=false&returnFaceLandmarks=false&returnFaceAttributes=emotion'
 headers = {
 	'Content-Type': 'application/octet-stream',
-	'Ocp-Apim-Subscription-Key':'e58d8c595daf4cdc949d411007e8d43f'
+	'Ocp-Apim-Subscription-Key':'82654e4421ec4270baedb5ecdbfd5c5e'
 }
 
 while True:
@@ -18,7 +18,7 @@ while True:
 		with open(images_dir + image, 'rb') as file:
 			data = file.read()
 			os.remove(images_dir + image)
-		response = requests.post(url, headers = headers, data = data)
+		response = requests.post(face_recognition_api_url, headers = headers, data = data)
 		if response.ok:
 			faces = response.json()
 			for face in faces:
@@ -26,10 +26,8 @@ while True:
 				face["timestamp"] = image[6:-4]
 				emotions.append(face)
 				print(face)
-		time.sleep(3)
-
+		time.sleep(1)
 	if emotions:
 		conn = mongo.connectDb()
 		mongo.save_emotion(conn, data = emotions)
 		conn.close()
-	time.sleep(10)
---
+++ demo/scripts/twitter_scraper.py
@@ -0,0 +1,30 @@
+import twitter
+import requests
+import json
+
+
+api = twitter.Api(consumer_key = "VVd2vJBNXOzbc8CgttEjCHcil",
+                  consumer_secret = "jERxFHuoUTxBqBFLQ1i0gZPZfv0IqlcIQOtrZ8ykwUtfnZ7j4I",
+                  application_only_auth = True)
+
+data = api.GetSearch(term = "#ah2018",
+					include_entities = False,
+					count = 100,
+					result_type = "recent",
+					return_json = True)
+
+
+tweets = [{"language" : "en", "id": i, "text": tweet["text"]}  for (i, tweet) in enumerate(data["statuses"])]
+#print(tweets)
+
+text_recognition_api_url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"
+headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': 'ac50b1b05f9a4bc680e843951d0ef474',
+}
+data = json.dumps({"documents": tweets})
+response = requests.post(text_recognition_api_url, headers = headers, data = data)
+if response.ok:
+	data = response.json()
+	scores = [sentiment["score"] for sentiment in data["documents"]]
+print(scores)
