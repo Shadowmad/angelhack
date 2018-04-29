02d1a54958cf956dbfb68e7d032e85eb75e13e69
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-04-29 04:02:53 -0700

    Added base64 upload

--- demo/views/images.py
+++ demo/views/images.py
@@ -1,6 +1,7 @@
 import os
 import time
 import hashlib
+import base64
 from demo import app
 from flask import Flask, request, redirect, url_for, send_from_directory
 
@@ -11,6 +12,13 @@ def get_extention(filename):
 
 def allowed_file(filename):
 	return '.' in filename and get_extention(filename) in ALLOWED_EXTENSIONS
+
+def save_image(file):
+	hash = hashlib.md5(file.filename.encode())
+	# newName = str(hash.hexdigest()) + '.' + get_extention(file.filename)
+	timestamp = time.strftime("%m%d%Y-%H:%m:%S")
+	newName =  str(hash.hexdigest()) + '-' + timestamp + '.' + get_extention(file.filename)
+	file.save(os.path.join(app.config['UPLOAD_FOLDER'], newName))	
 
 @app.route('/images/upload', methods=['POST', 'GET'])
 def	uploadImage():
@@ -34,3 +42,11 @@ def	uploadImage():
 			return 'Uploaded'
 		return 'LOL'
 	return send_from_directory('static', 'html/upload.html')
+
+@app.route('images/upload64', methods=['POST'])
+def uploadImage64():
+	file = request.files['file']
+	if file:
+		img = base64.decodebytes(file)
+		save_image(img)
+	return "Done"
