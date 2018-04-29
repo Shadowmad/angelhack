ac8ec0aa7859c1a3a66f2a4ed075a2fe4d8b6a55
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-28 18:59:15 -0700

    Added index route

--- demo/views/default.py
+++ demo/views/default.py
@@ -5,6 +5,10 @@ from demo import app
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/home')
+def home():
+    return app.send_static_file('html/index.html')
+
 @app.route('/static/<path:path>')
 def static_file(path):
     return app.send_static_file(path)
