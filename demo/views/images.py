d5b268f8ddb958182e7cfcaae20b1bae774f59ff
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-28 19:22:43 -0700

    Added hahsong

--- demo/views/images.py
+++ demo/views/images.py
@@ -1,12 +1,15 @@
 import os
+import hashlib
 from demo	import app
 from flask import Flask, request, redirect, url_for, send_from_directory
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+def get_extention(filename):
+	return filename.rsplit('.', 1)[1].lower() 
+
 def allowed_file(filename):
-	return '.' in filename and \
-		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	return '.' in filename and get_extention(filename) in ALLOWED_EXTENSIONS
 
 @app.route('/images/upload', methods=['POST', 'GET'])
 def	uploadImage():
@@ -21,7 +24,9 @@ def	uploadImage():
 			# flash('No selected file')
 			return redirect(request.url)
 		if file and allowed_file(file.filename):
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+			hash = hashlib.md5(file.filename.encode())
+			newName = str(hash.hexdigest()) + '.' + get_extention(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], newName))
 			# file.save(file.filename)
 			return 'Uploaded'
 		return 'LOL'
